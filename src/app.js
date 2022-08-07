806ca287ba2ae5b4ccf9ced4a0a8bb7c3880e82a
Emma Mansell <emilyjmansell@gmail.com>
2022-08-07 15:04:21 -0400

    penultimate draft

--- src/app.js
+++ src/app.js
@@ -1,20 +1,102 @@
-import React, { createContext, useEffect, useState } from "react";
-import { NewsContextProvider } from "./newsContext";
-import News from "./components/News";
-import NavBar from "./components/NavBar";
-import { ThemeProvider } from "@mui/material/styles";
+import { API_KEY } from "./config";
+import { makeStyles } from "@mui/styles";
 import { theme } from "./theme";
-import Footer from "./components/Footer";
+import { ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import NavBar from "./components/NavBar";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import Story from "./components/Story";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+
+  const getData = async () => {
+    const res = await axios.get(
+      `https://newsapi.org/v2/everything?domains=newyorker.com&apiKey=${API_KEY}`
+    );
+    const data = res.data.articles;
+    const slice = data.slice(offset, offset + perPage);
+    const articleData = slice.map((a) => <Story data={a} key={a.url} />);
+    setData(articleData);
+    setPageCount(Math.ceil(data.length / perPage));
+  };
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage * perPage);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [offset]);
+
+  const useStyles = makeStyles({
+    pagination: {
+      fontFamily: "Forum",
+      display: "flex",
+      listStyle: "none",
+      outline: "none",
+      "& li": {
+        padding: "15px",
+        "&.break": {
+          textDecoration: "none",
+          cursor: "pointer",
+        },
+        "&.previous.disabled": {
+          display: "none",
+        },
+        "&.selected": {
+          borderColor: "white",
+          outline: "solid",
+        },
+      },
+      "& li:hover": {
+        textDecoration: "underline",
+        cursor: "pointer",
+      },
+    },
+  });
+
+  const classes = useStyles();
+
   return (
-    <NewsContextProvider>
-      <ThemeProvider theme={theme}>
-        <NavBar />
-        <News />
-        <Footer />
-      </ThemeProvider>
-    </NewsContextProvider>
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      {data}
+      <Box
+        height={200}
+        width="100%"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bgcolor="primary.main"
+        color="white"
+        fontSize={24}
+      >
+        <ReactPaginate
+          previousLabel={"previous"}
+          nextLabel={"next "}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={2}
+          onPageChange={handlePageClick}
+          containerClassName={classes.pagination}
+        />
+        <div style={{ display: "flex", flexDirection: "row", padding: "20px" }}>
+          <CopyrightIcon />
+          <Typography variant="h2">Emma Mansell 2022</Typography>
+        </div>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { theme } from "./theme";
 
 const appRoot = document.getElementById("root");
-ReactDOM.render(<ThemeProvider theme={theme}> <App /> </ThemeProvider>, appRoot);
+ReactDOM.render(<App />, appRoot);
